675e11a40285004c9b0c27f0697765e4e214f0b4
Brian Matheny <bwmatheny@gmail.com>
2020-05-14 12:09:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,10 +80,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-	programmer_hash[:yukihiro_matsumoto] = :known_for
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 	programmer_hash[:yukihiro_matsumoto][:known_for][:languages] = ["LISP", "C"]
-
+programmer_hash
 end
 
 def changing_alan
